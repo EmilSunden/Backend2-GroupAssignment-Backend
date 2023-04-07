12de0612831ce5f5bff88a0f3a156a24c605ecff
elinhorn <elin.horn@hyperisland.se>
2023-04-07 16:31:30 +0200

    imported modules

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const server = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+server.listen(5050)
