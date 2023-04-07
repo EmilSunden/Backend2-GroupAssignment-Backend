77e9cca72f10acb8eb6770813eb3b1637142e84c
elinhorn <elin.horn@hyperisland.se>
2023-04-07 16:34:01 +0200

    created mongoDB connection with mongoose

--- src/index.js
+++ src/index.js
@@ -4,5 +4,20 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// Connect to MongoDB
+const database = process.env.MONGO_DB;
+
+
+const connect = async () => { 
+    try { 
+        await mongoose.connect(database, {useUnifideTopology: true, useNewUrlParser: true});
+        console.log('MongoDB connected');
+    } catch (err) { 
+        console.log(err)
+    }
+
+}
+
+connect()
 
 server.listen(5050)
