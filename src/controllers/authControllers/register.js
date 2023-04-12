69496398324586573319e74aa12366169f009adc
elinhorn <elin.horn@hyperisland.se>
2023-04-12 11:22:07 +0200

    create a new user and hash password

--- src/controllers/authControllers/register.js
+++ src/controllers/authControllers/register.js
@@ -1,9 +1,26 @@
 const bcrypt = require('bcrypt');
-const User = require('../../models/User');
-const express = require('express');
-const server = express();
-// validation add later
+const User = require('../../model/userModel');
+//validation add later
 
-// register user endpoint
+exports.register = async function register (req, res) {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({message: "Bad request", errors})
+        }
+        const {username, password} = req.body
+        const existUser = await User.findOne({username})
+        if (existUser) {
+            return res.status(400).json({message: `${username} already exist`})
+        }
+        const hashedPassword = await bcrypt.hash(password, 6)
+        const user = new User({username, password: hashedPassword})
 
+        await user.save()
+        return res.json({message: "User was created."})
+    } catch (err) {
+        console.log(err)
+        res.send({message: "Server error"})
+    }
 
+}
