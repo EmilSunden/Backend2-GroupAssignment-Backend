37329c104c631861fbc32de185e083e1e783e6b6
EmilSunden <emilsunjobb@gmail.com>
2023-04-12 13:30:36 +0200

    Deleted avatar from User schema

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,9 +1,8 @@
 const {Schema, model} = require('mongoose')
 
-const User = new Schema({
+const userSchema = new Schema({
     username:{type:String, required: true, unique: true},
     password:{type:String, required: true},
-    avatar:{type:String}
 });
 
-module.exports = model('Users', User)
+module.exports = model('User', userSchema)
