e998ce23c7bcf37a4d484e5804a670269b941b69
EmilSunden <emilsunjobb@gmail.com>
2023-04-13 11:09:11 +0200

    Added register after merge conflict

--- src/controllers/authControllers/register.js
+++ src/controllers/authControllers/register.js
@@ -5,5 +5,22 @@ const server = express();
 // validation add later
 
 // register user endpoint
+exports.register = async function register (req, res) {
+    try {
+        const {username, password} = req.body
+        const existUser = await User.findOne({username})
+        if (existUser) {
+            return res.status(400).json({message: `${username} already exist`})
+        }
+        const hashedPassword = await bcrypt.hash(password, 6)
+        const user = new User({username, password: hashedPassword})
 
+        await user.save()
+        return res.status(201).json({message: "User was created."})
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: "Server error"})
+    }
+
+}
 
