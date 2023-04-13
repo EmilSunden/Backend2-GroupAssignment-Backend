ab13cf70eb73b6f7a25ef60722dbb2c264131fe7
EmilSunden <emilsunjobb@gmail.com>
2023-04-13 14:56:10 +0200

    Added followers/following to User Schema

--- src/model/User.js
+++ src/model/User.js
@@ -3,6 +3,20 @@ const {Schema, model} = require('mongoose')
 const User = new Schema({
         username: {type: String, required: true, unique: true},
         password: {type: String, required: true},
+        followers: [
+            {
+                type: ObjectId,
+                ref: 'User',
+                required: true
+            }
+        ],
+        following: [
+            {
+                type: ObjectId,
+                ref: 'User',
+                required: true
+            }
+        ],
         avatar: {type: String}
     },
     {timestamps: true}
