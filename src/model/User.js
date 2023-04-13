ea788a4d83c1d860d7958b485180e0d0765f9537
EmilSunden <emilsunjobb@gmail.com>
2023-04-13 12:08:46 +0200

    Renamed property name

--- src/model/User.js
+++ src/model/User.js
@@ -1,7 +1,7 @@
 const {Schema, model} = require('mongoose')
 
 const User = new Schema({
-        email: {type: String, required: true, unique: true},
+        username: {type: String, required: true, unique: true},
         password: {type: String, required: true},
         avatar: {type: String}
     },
