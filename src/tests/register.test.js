529aded5481a0fe2e8899c686a561f0d64e9b3f1
elinhorn <elin.horn@hyperisland.se>
2023-04-13 09:41:41 +0200

    added math.random to username to make life easier when testing

--- src/tests/register.test.js
+++ src/tests/register.test.js
@@ -4,6 +4,7 @@ const { server } = require('../index');
 const { database } = require('../index');
 const mongoose = require('mongoose');
 
+const randomNum = Math.floor(Math.random() * 1000);
 
 describe("Testing server endpoint /register", () => {
 
@@ -28,13 +29,13 @@ describe("Testing server endpoint /register", () => {
 
     test("Should return 201 if user was created", async () => {
 
-        const response = await request(server).post('/api/auth/register').send({username: 'test', password: 'test'});
+        const response = await request(server).post('/api/auth/register').send({username: `UserNr${randomNum}`, password: 'test'});
 
         expect(response.status).toBe(201);
     });
 
     test("Should return 400 if user already exsist", async () => {
-        const response = await request(server).post('/api/auth/register').send({username: 'test', password: 'test'});
+        const response = await request(server).post('/api/auth/register').send({username: `UserNr${randomNum}`, password: 'test'});
 
         expect(response.status).toBe(400);
     });
