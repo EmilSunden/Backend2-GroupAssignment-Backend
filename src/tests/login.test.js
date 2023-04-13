d8565006cd61060d7ba0ee173dbbc1bc95c43de5
elinhorn <elin.horn@hyperisland.se>
2023-04-13 15:24:17 +0200

    created connection and close connection to database

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -3,3 +3,21 @@ const request = require('supertest');
 const { server } = require('../index');
 const { database } = require('../index');
 const mongoose = require('mongoose');
+
+describe("Testing server endpoint /register", () => {
+
+    beforeEach(async () => {
+        await mongoose.connect(database,
+            {
+                useUnifiedTopology: true,
+                useNewUrlParser: true,
+            });
+    }); 
+
+    afterEach(async () => {
+        await mongoose.connection.close();
+    });
+
+
+    
+});
