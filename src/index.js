59337f6614c090e093dde106549e60d625a54eb0
EmilSunden <emilsunjobb@gmail.com>
2023-04-17 16:58:44 +0200

    Moved the connect function to separate file and imported it. Added origin and credentials on cors

--- src/index.js
+++ src/index.js
@@ -1,36 +1,28 @@
 const express = require('express');
 const server = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
 const { postsRoutes } = require("./routes/postRouter");
 const { authRoutes } = require('./routes/authRoutes/authRoutes');
 require('dotenv').config();
 
+const { connect } = require('./config/db')
 
-server.use(cors());
+server.use(cors({
+    origin: true,
+    credentials: true
+}));
 server.use(express.json());
 
 server.use("/api/auth", postsRoutes)
 server.use("/api/auth", authRoutes)
 
 // Connect to MongoDB
-const database = process.env.MONGO_DB;
-
-
-const connect = async () => { 
-    try { 
-        await mongoose.connect(database);
-        console.log('MongoDB connected');
-    } catch (err) { 
-        console.log(err)
-    }
-
-}
-
 connect()
 
+server.listen(5050, () => {
+    console.log('Port running on http://localhost:5050')
+})
 
-
-server.listen(5050)
-
-module.exports = { connect, database, server };
+module.exports = { 
+    server 
+};
