f77f1f1d3f4bfcaa0263065e36033e72d902721b
EmilSunden <emilsunjobb@gmail.com>
2023-04-17 10:41:33 +0200

    Deleted followModel

--- src/model/followModel.js
+++
@@ -1,7 +0,0 @@
-const {Schema, model} = require('mongoose')
-
-const Follow = new Schema({
-    username:{ type: String, required: true, unique: true },
-});
-
-module.exports = model('Follow', Follow)
