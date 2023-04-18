09fb8057f8237ed8e2a7d40f69a584129a317c5a
EmilSunden <emilsunjobb@gmail.com>
2023-04-18 16:49:14 +0200

    Wrote test for /auth endpoint in authRoutes

---
+++ src/tests/auth.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const { server } = require('../index');
+const { database } = require('../config/db')
+const mongoose = require('mongoose');
+
+describe('GET /api/auth/auth', () => {
+  let token;
+
+  beforeAll(async () => {
+    await mongoose.connect(database);
+    console.log('MongoDB connected');
+
+    // Login the user and get the token
+    const response = await request(server)
+      .post('/api/auth/login')
+      .send({
+        username: 'trollet2',
+        password: 'trollet2',
+      });
+
+    token = response.body.token;
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  it('should respond with a cookie and a token', async () => {
+    const response = await request(server)
+      .get('/api/auth/auth')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body.token).toBeDefined();
+    expect(response.header['set-cookie']).toBeDefined();
+  });
+
+  it('should respond with an error if token is not provided', async () => {
+    const response = await request(server)
+      .get('/api/auth/auth');
+
+    expect(response.status).toBe(401);
+  });
+});
+
