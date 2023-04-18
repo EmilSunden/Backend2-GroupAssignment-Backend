1d2b60251c88a3add53d8f254a35e8c79aa1bf46
EmilSunden <emilsunjobb@gmail.com>
2023-04-18 16:48:16 +0200

    Removed console log

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,6 @@ const database = process.env.MONGO_DB;
 const connect = async () => { 
     try { 
         await mongoose.connect(database);
-        console.log('MongoDB connected');
     } catch (err) { 
         console.log(err)
     }
