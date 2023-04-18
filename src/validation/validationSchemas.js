6e5e545bedbb1faba4c6e7cc917de02388f40715
elinhorn <elin.horn@hyperisland.se>
2023-04-17 23:08:56 -0400

    created validation for auth register

--- src/validation/validationSchemas.js
+++ src/validation/validationSchemas.js
@@ -6,7 +6,13 @@ const postBodyValidation = Joi.object({
     user: Joi.string().required().length(24),
 });
 
+const registerValidation = Joi.object({
+    username: Joi.string().required().min(3).max(25),
+    password: Joi.string().required().min(6).max(25),
+});
+
 
 module.exports = {
-    postBodyValidation
+    postBodyValidation,
+    registerValidation
 }
