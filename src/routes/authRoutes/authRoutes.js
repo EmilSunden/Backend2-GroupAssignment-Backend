f282b023590383772001855f172ef5d86d81043d
w44t3r <vk.educate@protonmail.ch>
2023-04-19 17:11:54 +0200

    bug fixes - fixed middleware -- frontend, middleware was not configured correct. middleware jwt sign, should be like login sign.

--- src/routes/authRoutes/authRoutes.js
+++ src/routes/authRoutes/authRoutes.js
@@ -5,23 +5,27 @@ const { login } = require('../../controllers/authControllers/login');
 const User = require('../../model/User');
 const authMiddleware = require('../../middleware/authMiddleware')
 const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken')
 dotenv.config();
 const secretKey = process.env.secret_key
 
 
 authRoutes.post('/register', register);
 authRoutes.post('/login', login);
+
 authRoutes.get('/auth', authMiddleware,
     async (req, res) => {
         try {
             const user = await User.findOne({_id: req.user.id})
-            const token = jwt.sign({id: user.id}, secretKey, {expiresIn: "1h"})
-            return res.cookie("AuthCookies", token, {
-                maxAge: 1000 * 60 * 60, path: "api/auth", httpOnly: true
+            const token = jwt.sign({id:user._id, username: user.username}, secretKey, {expiresIn: "1h"})
+            return res.cookie("UserCookies", token, {
+                maxAge: 1000 * 60 * 60,
+                path: "api/auth/login",
+                httpOnly: true
             })
                 .status(200).json({
                     token, user: {
-                        status: "200", text: "Middleware Auth + Cookies.", id: user.id, username: user.username
+                        status: "200", text: "You are logged in", id: user._id, username: user.username
                     }
                 })
         } catch (err) {
