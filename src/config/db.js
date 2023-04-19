981996af679458c0343b4375427b3cb0cb74a5e4
EmilSunden <emilsunjobb@gmail.com>
2023-04-19 13:17:21 +0200

    Set strictquery to false

--- src/config/db.js
+++ src/config/db.js
@@ -7,6 +7,7 @@ const { MONGO_DB } = process.env;
 const connect = async () => { 
     try { 
         await mongoose.connect(MONGO_DB);
+        mongoose.set('strictQuery', false);
     } catch (err) { 
         console.log(err)
     }
