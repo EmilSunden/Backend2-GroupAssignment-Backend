31dec3d1e7319682a52feab1ef42242fb5515d1e
EmilSunden <emilsunjobb@gmail.com>
2023-04-20 15:59:40 +0200

    Updated get/posts/parameter

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -6,7 +6,7 @@ const router = new Router()
 
 router.post('/post', PostController.create);
 router.get('/posts', PostController.getAllUsersPosts );
-router.get('/:userId/posts', PostController.getUserPosts)
+router.get('/posts/:userId', PostController.getUserPosts)
 router.get('/post/:id', PostController.getOne);
 router.delete('/post/:id/delete', PostController.remove);
 router.patch('/post/:id', PostController.update);
