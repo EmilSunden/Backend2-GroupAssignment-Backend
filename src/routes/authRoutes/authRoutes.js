24af03e86bbe1140a0d0001d4bcfdc23b23168d3
EmilSunden <emilsunjobb@gmail.com>
2023-04-20 15:20:09 +0200

    Deleted duplicate code

--- src/routes/authRoutes/authRoutes.js
+++ src/routes/authRoutes/authRoutes.js
@@ -10,34 +10,4 @@ authRoutes.post('/register', register);
 authRoutes.post('/login', login);
 authRoutes.get('/auth', authMiddleware, getAuthCookie)
 
-const dotenv = require('dotenv');
-const jwt = require('jsonwebtoken')
-dotenv.config();
-const secretKey = process.env.secret_key
-
-
-authRoutes.post('/register', register);
-authRoutes.post('/login', login);
-
-authRoutes.get('/auth', authMiddleware,
-    async (req, res) => {
-        try {
-            const user = await User.findOne({_id: req.user.id})
-            const token = jwt.sign({id:user._id, username: user.username}, secretKey, {expiresIn: "1h"})
-            return res.cookie("UserCookies", token, {
-                maxAge: 1000 * 60 * 60,
-                path: "api/auth/login",
-                httpOnly: true
-            })
-                .status(200).json({
-                    token, user: {
-                        status: "200", text: "You are logged in", id: user._id, username: user.username
-                    }
-                })
-        } catch (err) {
-            console.log(err)
-        }
-    })
-
-
 exports.authRoutes = authRoutes;
