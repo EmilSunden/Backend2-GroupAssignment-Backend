5d4ad6e285865531576500f004c03634636679fd
EmilSunden <emilsunjobb@gmail.com>
2023-04-23 19:02:07 +0200

    Updated user model followers and following to take in user and username as objects

--- src/model/User.js
+++ src/model/User.js
@@ -1,23 +1,24 @@
-const {Schema, model, ObjectId} = require('mongoose')
+const { Schema, model, ObjectId } = require("mongoose");
 
-const User = new Schema({
-        username: {type: String, required: true, unique: true},
-        password: {type: String, required: true},
-        follower: [
-            {
-              type: ObjectId,
-              required: true,
-            },
-          ],
-          following: [
-            {
-              type: ObjectId,
-              required: true,
-            },
-          ],
-        avatar: {type: String}
-    },
-    {timestamps: true}
-)
+const User = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    followers: [
+      {
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+        username: { type: String },
+      },
+    ],
+    following: [
+      {
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+        username: { type: String },
+      },
+    ],
+    avatar: { type: String },
+  },
+  { timestamps: true }
+);
 
-module.exports = model('User', User)
+module.exports = model("User", User);
