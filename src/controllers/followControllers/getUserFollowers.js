a68e229d40ebc69ed978a9a92521b25da475bfa9
EmilSunden <emilsunjobb@gmail.com>
2023-04-23 19:00:30 +0200

    Updated req.params to username instead of userId

--- src/controllers/followControllers/getUserFollowers.js
+++ src/controllers/followControllers/getUserFollowers.js
@@ -1,15 +1,15 @@
 const User = require('../../model/User');
 
 async function getUserFollowers(req, res) {
-    const { userId } = req.params;
+    const { username } = req.params;
 
     try {
-        const user = await User.findById(userId).populate('follower');
+        const user = await User.findOne({username}).populate('followers');
 
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
-        res.status(200).json({ followers: user.follower })
+        res.status(200).json({ followers: user.followers })
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error' });
