ac611df10011ff0f3d781c63be5a7b9af1870a56
w44t3r <vk.educate@protonmail.ch>
2023-04-23 19:31:11 +0200

    Add port from .env and console.log it

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,22 @@
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
 const { MONGO_DB } = process.env;
+const { PORT } = process.env;
 
-const connect = async () => { 
-    try { 
+const connect = async () => {
+    try {
         await mongoose.connect(MONGO_DB);
         console.log('Connected successfully')
-        
-    } catch (err) { 
+        console.log(`Server started on PORT: ${PORT}`)
+
+    } catch (err) {
         console.log(err.message)
     }
 }
 
 module.exports = {
     connect,
-    MONGO_DB
+    MONGO_DB,
+    PORT
 }
