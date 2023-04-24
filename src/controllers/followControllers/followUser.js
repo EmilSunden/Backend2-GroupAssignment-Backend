5594c15a8d985943608ac535cdf36f5254973192
EmilSunden <emilsunjobb@gmail.com>
2023-04-25 00:11:02 +0200

    Changed to params instead of body

--- src/controllers/followControllers/followUser.js
+++ src/controllers/followControllers/followUser.js
@@ -2,11 +2,11 @@ const User = require('../../model/User');
 
 async function followUser(req, res) {
   const followerId  = req.user.id
-  const { followingId } = req.body;
+  const { id } = req.params;
 
   try {
     const followers = await User.findById(followerId);
-    const following = await User.findById(followingId);
+    const following = await User.findById(id);
 
     if (!followers) {
       return res.status(404).json({ message: 'Follower not found' });
@@ -16,12 +16,11 @@ async function followUser(req, res) {
       return res.status(404).json({ message: 'Following user not found' });
     }
 
-
-    if (followers.following.some((f) => f._id.toString() === followingId)) {
+    if (followers.following.some((f) => f.user.toString() === id)) {
       return res.status(409).json({ message: 'Already following this user' });
     }
 
-    followers.following.push({ user: followingId, username: following.username });
+    followers.following.push({ user: id, username: following.username });
     following.followers.push({ user: followerId, username: followers.username });
 
     await followers.save();
