c44a0ed3cff2e55269a338a9b1800013a1f1cde2
EmilSunden <emilsunjobb@gmail.com>
2023-04-25 00:11:24 +0200

    Updated follow route, added parameter

--- src/routes/followRoutes/followRoutes.js
+++ src/routes/followRoutes/followRoutes.js
@@ -4,7 +4,7 @@ const followerRoutes = new Router();
 const { getUserFollowers } = require('../../controllers/followControllers/getUserFollowers');
 const { followUser } = require('../../controllers/followControllers/followUser');
 
-followerRoutes.post('/follow', followUser);
+followerRoutes.post('/follow/:id', followUser);
 followerRoutes.get('/followers/:username', getUserFollowers);
 
 module.exports = {
