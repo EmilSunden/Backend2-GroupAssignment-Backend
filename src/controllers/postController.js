a37cab137d96a5110ff7d5e2badace46e475ba39
EmilSunden <emilsunjobb@gmail.com>
2023-04-24 14:53:50 +0200

    Added functionality for getting followers posts

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,149 +1,165 @@
 const Post = require("../model/Post");
 const User = require("../model/User");
-const {PostService} = require("../services/PostService");
-const {postBodyValidation} = require("../validation/validationSchemas");
-const {isMongoId} = require("validator");
-
+const { PostService } = require("../services/PostService");
+const { postBodyValidation } = require("../validation/validationSchemas");
+const { isMongoId } = require("validator");
 
 module.exports.create = async (req, res) => {
-    const {title, text} = req.body;
-    const bodyRequestData = {
-        title,
-        text,
-        user: req.user.id,
-           
-    };
+  const { title, text } = req.body;
+  const bodyRequestData = {
+    title,
+    text,
+    user: req.user.id,
+  };
 
-
-    const validation = await postBodyValidation.validate(bodyRequestData);
-    if (validation.error) {
-        return res.status(400).json(validation.error.details[0].message);
-    } else {
-        const post = await PostService.create(validation.value);
-        try {
-            const result = await PostService.save(post);
-            if (result) {
-                return res.status(201).json(result);
-            } else {
-                return res.status(400).json({error: `Can't create a post`});
-            }
-        } catch (err) {
-            console.log(err);
-            return res.status(500).json({error: "Internal Server Error"});
-        }
+  const validation = await postBodyValidation.validate(bodyRequestData);
+  if (validation.error) {
+    return res.status(400).json(validation.error.details[0].message);
+  } else {
+    const post = await PostService.create(validation.value);
+    try {
+      const result = await PostService.save(post);
+      if (result) {
+        return res.status(201).json(result);
+      } else {
+        return res.status(400).json({ error: `Can't create a post` });
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
+  }
 };
 
 module.exports.getAllUsersPosts = async (req, res) => {
-    try {
-        const posts = await Post.find({}).populate({path: 'user', select: "-password"});
-        if (posts) {
-            res.send(posts);
-        } else {
-            res.status(404).json({message: "No posts found"});
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({message: `Can't get post`});
+  try {
+    const posts = await Post.find({}).populate({
+      path: "user",
+      select: "-password",
+    });
+    if (posts) {
+      res.send(posts);
+    } else {
+      res.status(404).json({ message: "No posts found" });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Can't get post` });
+  }
 };
 
 module.exports.getUserPosts = async (req, res) => {
-    try {    
-        const { username } = req.params
-        const user = await User.find({username})
-        
-        if (user) {
-            const posts = await Post.find({user})
-                .populate({
-                    path: "user",
-                    select: "username",
-            })
-        
-            if (posts) {
-                console.log(`Found ${posts.length} posts by user ${username}.`);
-                res.send(posts)
-            } else {
-                res.status(404).json({ message: 'No posts found'})
-            }
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({message: `Can't get posts for that user`});
+  try {
+    const { username } = req.params;
+    const user = await User.find({ username });
+
+    if (user) {
+      const posts = await Post.find({ user }).populate({
+        path: "user",
+        select: "username",
+      });
+
+      if (posts) {
+        console.log(`Found ${posts.length} posts by user ${username}.`);
+        res.send(posts);
+      } else {
+        res.status(404).json({ message: "No posts found" });
+      }
     }
-}
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Can't get posts for that user` });
+  }
+};
+
+module.exports.getFollowersPosts = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const result = await Post.find({ _id: id }).populate({
+      path: "user",
+      select: "followers",
+    });
+    if (result) {
+      res.status(200).json({ result });
+    } else {
+      res.status(404).json({ message: "No posts found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Can't get posts for that user` });
+  }
+};
 
 module.exports.getOne = async (req, res) => {
-    try {
-        const postId = req.params.id;
-        if (isMongoId(postId)) {
-            const postGetOne = await PostService.findPost(postId);
-            if (postGetOne) {
-                res.send(postGetOne);
-            } else {
-                return res.status(500).json({message: `Sry, can't get post`});
-            }
-        } else {
-            res.status(404).json({message: "id is not correct"});
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({message: `Can't get post`});
+  try {
+    const postId = req.params.id;
+    if (isMongoId(postId)) {
+      const postGetOne = await PostService.findPost(postId);
+      if (postGetOne) {
+        res.send(postGetOne);
+      } else {
+        return res.status(500).json({ message: `Sry, can't get post` });
+      }
+    } else {
+      res.status(404).json({ message: "id is not correct" });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Can't get post` });
+  }
 };
 
 module.exports.remove = async (req, res) => {
-    try {
-        const postId = req.params.id;
-        if (isMongoId(postId)) {
-            const postRemove = await PostService.remove(postId);
+  try {
+    const postId = req.params.id;
+    if (isMongoId(postId)) {
+      const postRemove = await PostService.remove(postId);
 
-            if (postRemove) {
-                res.status(200).json({message: "Deleted"});
-            } else if (postRemove === null) {
-                res.status(404).json({message: `Sry, post not found`});
-            } else {
-                res.status(500).json({message: "Internal Server Error"});
-            }
-        } else {
-            return res.status(404).json({message: "id is not correct"});
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({message: `Internal Server Error`});
+      if (postRemove) {
+        res.status(200).json({ message: "Deleted" });
+      } else if (postRemove === null) {
+        res.status(404).json({ message: `Sry, post not found` });
+      } else {
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    } else {
+      return res.status(404).json({ message: "id is not correct" });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Internal Server Error` });
+  }
 };
 
 module.exports.update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const { title, text } = req.body;
+    const bodyRequestData = {
+      title,
+      text,
+      user: req.user.id,
+    };
+    const validation = await postBodyValidation.validate(bodyRequestData);
+    if (validation.error) {
+      return res.status(400).json(validation.error.details[0].message);
+    } else {
+      const validation = await postBodyValidation.validate(bodyRequestData);
 
-    try {
-        const postId = req.params.id;
-        const {title, text} = req.body;
-        const bodyRequestData = {
-            title,
-            text,
-            user: req.user.id,
-                
-        };
-        const validation = await postBodyValidation.validate(bodyRequestData);
-        if (validation.error) {
-            return res.status(400).json(validation.error.details[0].message);
+      if (validation.error) {
+        return res.status(400).json(validation.error.details[0].message);
+      } else {
+        const updatedPost = await PostService.update(postId, validation.value);
+        if (updatedPost) {
+          res.status(200).json({ message: "Post updated" });
         } else {
-            const validation = await postBodyValidation.validate(bodyRequestData);
-
-            if (validation.error) {
-                return res.status(400).json(validation.error.details[0].message);
-            } else {
-                const updatedPost = await PostService.update(postId, validation.value);
-                if (updatedPost) {
-                    res.status(200).json({message: "Post updated"});
-                } else {
-                    res.status(500).json({message: "Server Error"});
-                }
-            }
+          res.status(500).json({ message: "Server Error" });
         }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({message: `Can't updated post, server error`});
+      }
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Can't updated post, server error` });
+  }
 };
