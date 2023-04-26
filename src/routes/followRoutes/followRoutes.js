8dd1650c239a5b1b855ca913cef636f3aebee221
EmilSunden <emilsunjobb@gmail.com>
2023-04-26 20:48:52 +0200

    Added unfollow route

--- src/routes/followRoutes/followRoutes.js
+++ src/routes/followRoutes/followRoutes.js
@@ -4,8 +4,11 @@ const PostController = require('../../controllers/postController');
 
 const { getUserFollowers } = require('../../controllers/followControllers/getUserFollowers');
 const { followUser } = require('../../controllers/followControllers/followUser');
+const { unfollowUser } = require('../../controllers/followControllers/unFollowUser')
 
 followerRoutes.post('/follow/:id', followUser);
+followerRoutes.post('/unfollow/:id', unfollowUser)
+
 followerRoutes.get('/followers/:username', getUserFollowers);
 followerRoutes.get('/following/:id', PostController.getFollowingPosts)
 
